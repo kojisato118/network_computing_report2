1b12424f62bbdbc90016c529b5c4320b6a2d58b7
kojisato118 <k.s.9190118@gmail.com>
2016-06-05 16:18:56 +0900

    一旦コミット

--- tcp_concurrent_server.c
+++ tcp_concurrent_server.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-  pid_t pid;
+  pid_t pid, pid2;
   int listenfd, connfd;
   struct sockaddr_in addr;
   struct sockaddr_in client;
@@ -20,38 +20,56 @@ int main(int argc, char *argv[])
   int cli;
   char *cli_addr;
 
-  listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(12345);
-  addr.sin_addr.s_addr = INADDR_ANY;
-  bind(listenfd, (struct sockaddr *)&addr, sizeof(addr));
-  listen(listenfd, LISTENQ);
+  int sock[16];
+  int n;
+  struct addrinfo hints, *res0, *res;
+  int yes = 1;
 
-  for ( ; ; ) {
-    len = sizeof(client);
-    connfd = accept(listenfd, (struct sockaddr *)&client, &len);
-    if ( (pid = fork() ) == 0 ) {
-      close(listenfd);
-      printf("accepted connection from %s, port=%d\n",
-              inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-      write(connfd, "HELLO\n", 6);
+  char buff[256];
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_flags = AI_PASSIVE;
+  hints.ai_socktype = SOCK_STREAM;
+  getaddrinfo(NULL, "12345", &hints, &res0);
+
+  res = res0;
+  n = 0;
+
+  printf("start while!\n");
+  while(res != NULL) {
+    printf("%d回目\n", n);
+    getnameinfo(res->ai_addr, res->ai_addrlen, buff, sizeof(buff), NULL, 0, NI_NUMERICHOST);
+    printf("IP address : %s¥n", buff);
+
+//    sock[n] = socket(res->ai_family, res->ai_socktype, 0);
+//    setsockopt(sock[n], IPPROTO_IPV6, IPV6_V6ONLY, (char *)&yes, sizeof(yes));
+//    bind(sock[n], res->ai_addr, res->ai_addrlen);
+//    listen(sock[n], LISTENQ);
+//    
+//    if ( (pid = fork() ) == 0) {
+//      len = sizeof(client);
+//      connfd = accept(sock[n], (struct sockaddr *)&client, &len);
+//      if ( (pid2 = fork() ) == 0 ) {
+//        close(sock[n]);
+//        printf("accepted connection from %s, port=%d\n",
+//                inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+//        write(connfd, "HELLO\n", 6);
+//        
+//        close(connfd); exit(0);
+//      }
+//      close(connfd);
+//    }
+    res = res->ai_next;
       
-      close(connfd); exit(0);
-    }
-    close(connfd);
+    if(++n == 16) break;
   }
-//  while (1) {
-//    len = sizeof(client);
-//    cli = accept(sock, (struct sockaddr *)&client, &len);
-//    printf("accepted connection from %s, port=%d\n",
-//              inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-//
-//    write(cli, "HELLO\n", 6);
-// 
-//    close(cli);
+
+//  freeaddrinfo(res0);
+//  
+//  for(n = 0; n < 16; n++){
+//    close(sock[n]);
 //  }
-
-  close(listenfd);
-
+  
   return 0;
 }

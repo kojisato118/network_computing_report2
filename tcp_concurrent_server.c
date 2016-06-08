64c651eb41e4329da214229a8f4fc956bbfdd820
kojisato118 <k.s.9190118@gmail.com>
2016-06-08 09:07:54 +0900

    簡易的なsignalを実装

--- tcp_concurrent_server.c
+++ tcp_concurrent_server.c
@@ -7,8 +7,18 @@
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define LISTENQ 100
+#define MAX_PROCESS 16
+
+void sig_child(int signo);
+void sig_term(int signo); 
+void sig_quit(int signo);
+
+int pids[MAX_PROCESS];
+void init_pids();
+void kill_all_pids();
 
 int main(int argc, char *argv[])
 {
@@ -20,12 +30,18 @@ int main(int argc, char *argv[])
   int cli;
   char *cli_addr;
 
-  int sock[16];
+  int sock[MAX_PROCESS];
   int n;
   struct addrinfo hints, *res0, *res;
   int yes = 1;
 
   char buff[256];
+  
+  signal(SIGCHLD, sig_child); 
+  signal(SIGTERM, sig_term);
+  signal(SIGQUIT, sig_quit);
+
+  init_pids();
 
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC;
@@ -47,24 +63,45 @@ int main(int argc, char *argv[])
     bind(sock[n], res->ai_addr, res->ai_addrlen);
     
     if ( (pid = fork() ) == 0) {
+      //listen用プロセス
       listen(sock[n], LISTENQ);
-      len = sizeof(client);
-      connfd = accept(sock[n], (struct sockaddr *)&client, &len);
-      if ( (pid2 = fork() ) == 0 ) {
-        close(sock[n]);
-        printf("accepted connection from %s, port=%d\n",
-                inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-        write(connfd, "HELLO\n", 6);
-        
-        close(connfd); exit(0);
+      while(1){
+        len = sizeof(client);
+        connfd = accept(sock[n], (struct sockaddr *)&client, &len);
+        sleep(10);
+        if ( (pid2 = fork() ) == 0 ) {
+          close(sock[n]);
+          printf("accepted connection from %s, port=%d at PIDD=%d\n",
+                  inet_ntoa(client.sin_addr), ntohs(client.sin_port), getpid());
+          write(connfd, "HELLO\n", 6);
+          
+          close(connfd); exit(0);
+        }else{
+           if (pid2 != -1) {
+             // listen用プロセス
+           } else {
+             perror("pid2 fork");
+           }
+        }
+        close(connfd);
       }
-      close(connfd);
+    }else{
+      if(pid != -1){
+        //大元のプロセス
+        pids[n] = pid;
+      }else{
+        perror("pid fork");
+      }
     }
     res = res->ai_next;
       
-    if(++n == 16) break;
+    if(++n == MAX_PROCESS) break;
   }
 
+
+  while(1){
+    // listenをforkした子プロセスでやっているため、大元のプロセスが終了するのを防ぐため
+  }
 //  freeaddrinfo(res0);
 //  
 //  for(n = 0; n < 16; n++){
@@ -72,4 +109,48 @@ int main(int argc, char *argv[])
 //  }
   
   return 0;
+}
+
+void sig_child(int signo)
+{
+  printf("accept SIGCHLD at PID: %d\n", getpid());
+  int pid, status;
+  pid = wait(&status);
+  printf("PID: %d, terminated\n", pid);
+} 
+
+void sig_term(int signo)
+{
+  printf("accept SIGTERM at PID: %d\n", getpid());
+  kill_all_pids();
+}
+
+void sig_quit(int signo)
+{
+  printf("accept SIGQUIT at PID: %d\n", getpid());
+  kill_all_pids();
+}
+
+void init_pids()
+{
+  int i;
+
+  for(i = 0; i < MAX_PROCESS; i++){
+    pids[i] = -1;
+  }
+}
+
+void kill_all_pids()
+{
+  int i, pid, status;
+
+  for(i = 0; i < MAX_PROCESS; i++){
+    if(pids[i] > 0){
+      kill(pids[i], SIGTERM);
+      pids[i] = -1;
+      pid = wait(&status);
+      printf("PID: %d, terminated\n", pid);
+    }
+  }
+ 
 }

c0740d29923f6b6ea082d5835fb2d413da485fa9
kojisato118 <k.s.9190118@gmail.com>
2016-06-08 22:21:38 +0900

    メソッド切り分け

--- tcp_concurrent_server.c
+++ tcp_concurrent_server.c
@@ -22,13 +22,12 @@ void sig_kill(int signo);
 
 void kill_all_pids();
 
+void fork_for_listen(int sock);
+void fork_for_client(int sock, int connfd, struct sockaddr_in client);
+
 int main(int argc, char *argv[])
 {
-  pid_t pid, pid2;
-  int listenfd, connfd;
   struct sockaddr_in addr;
-  struct sockaddr_in client;
-  socklen_t len;
   int cli;
   char *cli_addr;
 
@@ -38,8 +37,6 @@ int main(int argc, char *argv[])
   int yes = 1;
 
   char buff[BUFF_SIZE];
-  char inbuf[BUFF_SIZE];
-  char obuf[BUFF_SIZE];
 
   signal(SIGCHLD, sig_child); 
   signal(SIGTERM, sig_term);
@@ -72,60 +69,9 @@ int main(int argc, char *argv[])
       perror("bind");
       continue;
     }
-    
-    if ( (pid = fork() ) == 0) {
-      //listen用プロセス
-      for(int i = 0; i < n; i++){
-        close(sock[i]);
-      }
 
-      if(listen(sock[n], LISTENQ) < 0){
-        perror("listen");
-        continue;
-      }
+    fork_for_listen(sock[n]);
 
-      while(1){
-        len = sizeof(client);
-        connfd = accept(sock[n], (struct sockaddr *)&client, &len);
-        
-        if(connfd < 0){
-          perror("accept");
-          continue;
-        }        
-        // TODO: 今のままだと無限にプロセス生成になっちゃうから、制限をつける
-        if ( (pid2 = fork() ) == 0 ) {
-          close(sock[n]);
-          printf("accepted connection from %s, port=%d at PIDD=%d\n",
-                  inet_ntoa(client.sin_addr), ntohs(client.sin_port), getpid());
-          memset(&inbuf, 0, sizeof(inbuf));
-          recv(connfd, inbuf, sizeof(inbuf), 0);
-          printf("%s", inbuf);
-         
-          memset(&obuf, 0, sizeof(obuf));
-          snprintf(obuf, sizeof(obuf),
-                "HTTP/1.0 200 OK\r\n"
-                "Content-Type: text/html\r\n"
-                 "\r\n"
-                 "<font color=red><h1>HELLO</h1></font>\r\n");
-          send(connfd, obuf, (int)strlen(obuf), 0);
-          
-          close(connfd); exit(0);
-        }else{
-           if (pid2 != -1) {
-             // listen用プロセス
-           } else {
-             perror("pid2 fork");
-           }
-        }
-        close(connfd);
-      }
-    }else{
-      if(pid != -1){
-        //大元のプロセス
-      }else{
-        perror("pid fork");
-      }
-    }
     res = res->ai_next;
       
     if(++n == MAX_PROTOCOL) break;
@@ -137,6 +83,75 @@ int main(int argc, char *argv[])
   }
   
   return 0;
+}
+
+void fork_for_listen(int sock)
+{
+  pid_t pid;
+  struct sockaddr_in client;
+  socklen_t len;
+  int connfd;
+
+  if ( (pid = fork() ) == 0) {
+    //listen用プロセス
+    if(listen(sock, LISTENQ) < 0){
+      perror("listen");
+      return;
+    }
+
+    while(1){
+      len = sizeof(client);
+      connfd = accept(sock, (struct sockaddr *)&client, &len);
+      if(connfd < 0){
+        perror("accept");
+        continue;
+      } 
+
+      fork_for_client(sock, connfd, client);
+
+      close(connfd);
+    }
+  }else{
+    if(pid != -1){
+      //大元のプロセス
+    }else{
+      perror("pid fork");
+    }
+  }
+
+}
+
+void fork_for_client(int sock, int connfd, struct sockaddr_in client)
+{
+  pid_t pid;
+  char inbuf[BUFF_SIZE];
+  char obuf[BUFF_SIZE];
+
+// TODO: 今のままだと無限にプロセス生成になっちゃうから、制限をつける
+  if ( (pid = fork() ) == 0 ) {
+    close(sock);
+    printf("accepted connection from %s, port=%d at PIDD=%d\n",
+           inet_ntoa(client.sin_addr), ntohs(client.sin_port), getpid());
+    memset(&inbuf, 0, sizeof(inbuf));
+    recv(connfd, inbuf, sizeof(inbuf), 0);
+    printf("%s", inbuf);
+  
+    memset(&obuf, 0, sizeof(obuf));
+    snprintf(obuf, sizeof(obuf),
+         "HTTP/1.0 200 OK\r\n"
+         "Content-Type: text/html\r\n"
+          "\r\n"
+          "<font color=red><h1>HELLO</h1></font>\r\n");
+    send(connfd, obuf, (int)strlen(obuf), 0);
+   
+    close(connfd); exit(0);
+  }else{
+    if (pid != -1) {
+      // listen用プロセス
+    } else {
+      perror("pid fork");
+    }
+  }
 }
 
 void sig_child(int signo)
